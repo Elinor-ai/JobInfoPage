9438f1dc67c6ff6b3650a2a6e0190be1ea2b3626
Elinor-ai <elinor@botson.ai>
2025-09-17 15:52:19 +0300

    Add redis cache of vareint

--- server/redis.js
+++ server/redis.js
@@ -1,3 +1,7 @@
+// This module initializes a Redis client. 
+// It reads the Redis connection URL from an environment variable and exports the connected client for use in other modules. 
+// The module handles connection and error events gracefully
+
 const { createClient } = require('redis');
 
 const redisClient = createClient({
